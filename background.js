bc9fa72343b91121c27aeab66c035a5238fd4a18
Dominik <23344776+blr-dev@users.noreply.github.com>
2023-06-21 21:52:44 +0200

    Refactor background.js

--- background.js
+++ background.js
@@ -1,32 +1,29 @@
-var filter = {
-	"urls": ["*://*/*"]
+const filter = {
+  urls: ["*://*/*"]
 };
 
-var pageLoaded = false;
+let pageLoaded = false;
 
 function randomRange(min, max) {
-  return Math.floor(Math.random() * (max-min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-chrome.webRequest.onBeforeRequest.addListener(function(details){
-	pageLoaded = false;
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  pageLoaded = false;
 }, filter);
 
-chrome.webRequest.onErrorOccurred.addListener(function(details){
-	if (details.type == "main_frame") {
-		if( details.tabId != -1 ) {
-			if(pageLoaded == false){
-				console.log("Refreshing..");
-				setTimeout(function(){		
-					chrome.tabs.reload(details.tabId, {bypassCache: false});
-					pageLoaded = false;
-				}, randomRange(2000, 6000));
-			}
-		}
-	}
+chrome.webRequest.onErrorOccurred.addListener(function(details) {
+  if (details.type === "main_frame" && details.tabId !== -1) {
+    if (!pageLoaded) {
+      console.log("Refreshing..");
+      setTimeout(function() {
+        chrome.tabs.reload(details.tabId, { bypassCache: false });
+        pageLoaded = false;
+      }, randomRange(2000, 6000));
+    }
+  }
 }, filter);
 
-chrome.webRequest.onCompleted.addListener(function(details){
-	pageLoaded = true;
+chrome.webRequest.onCompleted.addListener(function(details) {
+  pageLoaded = true;
 }, filter);
-
